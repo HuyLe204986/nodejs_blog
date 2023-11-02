efb127470b0f79949dcf98e7687414da97344200
huy.ltn204986 <huy.ltn204986@sis.hust.edu.vn>
2023-11-02 21:55:52 +0700

    format code

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-// import { engine } from 'express-handlebars'
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+const route = require('./routes');
 
-const app = express()
-const port = 3000
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 //http logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 //Template engine (handlebars)
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname,'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
+//route init
+        route(app);
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('new');
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+            app.listen(port, () => {
+                console.log(`Example app listening on port ${port}`);
+            });
